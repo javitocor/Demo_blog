cea9566866e5f5478c5a5caad549c88ea1eb0271
javitocor <javitocor@gmail.com>
2020-04-23 17:30:56 +0200

    Added validations to the models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,5 @@
 class Comment < ApplicationRecord
     belongs_to :post
+    validates :name, presence: true
+    validates :content, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
+    validates :title, presence: true, length: { maximum:30 }
+    validates :content, presence: true, length: { minimun: 50 }
 end
