17a16ff99c60b6825ad6215ab9a50cd7f011c7d4
javitocor <javitocor@gmail.com>
2020-04-23 16:51:03 +0200

    Added forms for the comments, create and destroy actions and added a migration to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,10 @@
 class CommentsController < ApplicationController
+    def create 
+        @post = Post.find(params[:post_id])
+        @comment = @post.comments.create(params[:comment]).permit(:name, :comment)
+        redirect_to post_path(@post)
+    end
+
+    def destroy 
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_142722) do
+ActiveRecord::Schema.define(version: 2020_04_23_143250) do
 
   create_table "comments", force: :cascade do |t|
     t.string "name"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "post_id"
   end
 
   create_table "posts", force: :cascade do |t|
