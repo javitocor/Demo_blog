43517160c7f0e54dee552790b2c13c4167f141ca
javitocor <javitocor@gmail.com>
2020-04-23 16:00:30 +0200

    Modified show views and added edit and update actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,19 @@ class PostsController < ApplicationController
         @post = Post.find(params[:id])
     end
 
+    def edit
+        @post = Post.find(params[:id]) 
+    end
+
+    def update 
+        @post = Post.find(params[:id])
+        if @post.update(post_params)
+            redirect_to @post 
+        else
+            render :edit 
+        end
+    end
+
     private
     def post_params
         params.require(:post).permit(:title, :content)
