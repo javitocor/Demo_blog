11c7544a2f51d6c7551f7cdc1775162c58f5b0e7
javitocor <javitocor@gmail.com>
2020-04-23 16:28:26 +0200

    Created model and controller for comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_23_131015) do
+ActiveRecord::Schema.define(version: 2020_04_23_142722) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
