56a9ba35146c702317feca61388da8535bc1d2a0
javitocor <javitocor@gmail.com>
2020-04-23 15:54:39 +0200

    Created views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,28 @@
 class PostsController < ApplicationController
     def index 
+        @posts = Post.all
     end
 
     def new 
+        @post = Post.new
     end
 
     def create
+        @post  = Post.new(post_params)
+        if @post.save
+            redirect_to @post
+        else
+            render :new
+        end
     end
+
+    def show 
+        @post = Post.find(params[:id])
+    end
+
+    private
+    def post_params
+        params.require(:post).permit(:title, :content)
+    end
+
 end
