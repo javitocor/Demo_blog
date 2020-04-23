acb89419b19833828831ad9b14d1811e7d6c5e9c
javitocor <javitocor@gmail.com>
2020-04-23 17:37:32 +0200

    Added errors to display

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { maximum:30 }
-    validates :content, presence: true, length: { minimun: 50 }
+    validates :content, presence: true, length: { minimun: 50, maximum: 5500 }
 end
